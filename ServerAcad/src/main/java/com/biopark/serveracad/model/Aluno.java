c9cb5c73e13b65988aae16b2ba91767efccf564c
Ifta <iftarodrigues@gmail.com>
2021-12-10 02:08:09 -0300

    Criação da classe aluno e configuração do pom.xml

---
+++ ServerAcad/src/main/java/com/biopark/serveracad/model/Aluno.java
@@ -0,0 +1,48 @@
+package com.biopark.serveracad.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author ifta
+ */
+@Entity
+public class Aluno implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
+}
