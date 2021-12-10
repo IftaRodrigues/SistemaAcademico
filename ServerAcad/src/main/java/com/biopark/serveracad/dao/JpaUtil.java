d512e03915461b54c0da88cc5a4683420350580b
Ifta <iftarodrigues@gmail.com>
2021-12-10 18:17:49 -0300

    Criação do JpaUtil e correções de injeção

---
+++ ServerAcad/src/main/java/com/biopark/serveracad/dao/JpaUtil.java
@@ -0,0 +1,37 @@
+package com.biopark.serveracad.dao;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author ifta
+ */
+@Dependent
+public class JpaUtil {
+    
+    @ApplicationScoped
+    @Produces
+    public EntityManagerFactory factory(){
+        return Persistence.createEntityManagerFactory("conexao");
+    }
+    
+    @Produces
+    public EntityManager manager(EntityManagerFactory factory){
+        return factory.createEntityManager();
+    }
+    
+    public void close(@Disposes EntityManagerFactory factory){
+        factory.close();
+    }
+    
+    public void close(@Disposes EntityManager manager){
+        manager.clear();
+    }
+    
+}
